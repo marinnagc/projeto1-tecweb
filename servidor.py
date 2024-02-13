dc1f4e909ae139804578415d4efdac30522e4837
Marinna Cesar <marinnagc@al.insper.edu.br>
2024-02-13 18:56:29 -0300

    atualizações

--- servidor.py
+++ servidor.py
@@ -1,24 +1,11 @@
 import socket
+from pathlib import Path
+from utils import extract_route, read_file, load_data, load_template
+from views import index
 
+CUR_DIR = Path(__file__).parent
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8080
-
-RESPONSE_TEMPLATE = '''HTTP/1.1 200 OK
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Get-it</title>
-</head>
-<body>
-
-<h1>Get-it</h1>
-<p>Como o Post-it, mas com outro verbo</p>
-
-</body>
-</html>
-'''
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -34,7 +21,21 @@ while True:
     print('*'*100)
     print(request)
 
-    client_connection.sendall(RESPONSE_TEMPLATE.encode())
+    route = extract_route(request)
+    filepath = CUR_DIR / route
+    if filepath.is_file():
+       # ERA ASSIM: response = 'HTTP/1.1 200 OK\n\n'.encode() + read_file(filepath)
+        response = read_file(filepath)
+    elif route == '':
+        response = index()
+    else:
+        response = bytes()
+    client_connection.sendall('HTTP/1.1 200 OK\n\n'.encode() + response)
+       # ERA ASSIM: response = RESPONSE_TEMPLATE.format(notes=notes).encode()
+    # ERA ASSIM: client_connection.sendall(response)
+
+
+    # ERA ASSIM: client_connection.sendall(RESPONSE_TEMPLATE.encode())
 
     client_connection.close()
 
